333905c7460535cfbbb7e383d93c8597a8ba71fa
deepspsd <dp3189756@gmail.com>
2025-07-26 23:17:16 +0530

    Delete extention/util.js

--- extention/util.js
+++
@@ -1,7 +0,0 @@
-// Utility functions for WebShield Extension
-export function getToken(cb) {
-  chrome.storage.sync.get('token', ({ token }) => cb(token));
-}
-export function setToken(token, cb) {
-  chrome.storage.sync.set({ token }, cb);
-} 
